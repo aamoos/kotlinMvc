9bd1cf3becfabece2b8aec343603ae2a441b6e16
kimjaesung <aamoos@naver.com>
2024-06-05 10:40:35 +0900

    POST

--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,8 +1,14 @@
 package com.example.mvc.model.http
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest(
     val name:String?=null,
     val age:Int?=null,
     val email:String?=null,
-    val address:String?=null
+    val address:String?=null,
+    val phoneNumber:String?=null    //phone_number
 )
