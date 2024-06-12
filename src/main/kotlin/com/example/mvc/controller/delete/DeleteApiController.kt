78cbc5f3238ed813c1ea30edbc5cfb5d315e7a5b
김재성 <aamoos@openmate.co.kr>
2024-06-12 10:49:55 +0900

    JSR-380 Bean Validation

--- src/main/kotlin/com/example/mvc/controller/delete/DeleteApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/delete/DeleteApiController.kt
@@ -1,5 +1,9 @@
 package com.example.mvc.controller.delete
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
+@Validated
 class DeleteApiController {
 
     //1. path variable
@@ -17,6 +22,9 @@ class DeleteApiController {
     @DeleteMapping(path = ["/delete-mapping"])
     fun deleteMapping(
         @RequestParam(value = "name") _name: String,
+
+        @NotNull(message = "age 값이 누락되었습니다.")
+        @Min(value = 20, message = "age는 20보다 커야 합니다.")
         @RequestParam(name = "age") _age: Int
     ): String{
         println(_name)
@@ -25,7 +33,14 @@ class DeleteApiController {
     }
 
     @DeleteMapping(path = ["/delete-mapping/{name}/{age}"])
-    fun deleteMappingPath(@PathVariable(value = "name") _name:String, @PathVariable(name = "age") _age:Int): String {
+    fun deleteMappingPath(@PathVariable(value = "name")
+                          @Size(min = 2, max = 5, message = "name의 길이는 2~5")
+                          @NotNull
+                          _name:String, //aa ~ aaaaa
+
+            @NotNull(message = "age 값이 누락되었습니다.")
+            @Min(value = 20, message = "age는 20보다 커야 합니다.")
+            @PathVariable(name = "age") _age:Int): String {
         println(_name)
         println(_age)
         return _name + " " + _age
--- src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -3,11 +3,11 @@ package com.example.mvc.controller.put
 import com.example.mvc.model.http.Result
 import com.example.mvc.model.http.UserRequest
 import com.example.mvc.model.http.UserResponse
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
@@ -24,37 +24,52 @@ class PutApiController {
     }
 
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
-        //0. Response
-        return UserResponse().apply {
-            //1. request
-            this.result = Result().apply{
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
+
+        if(bindingResult.hasErrors()){
+            //error를 가지고있는가?
+            //500 error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field+" : "+message+"\n")
             }
-        }.apply {
-            this.description = "~~~~~~~~~~"
-
-        }.apply {
-            val userList = mutableListOf<UserRequest>()
-            userList.add(userRequest)
-            userList.add(UserRequest().apply {
-                this.name = "a"
-                this.age = 10
-                this.email = "a@gmail.com"
-                this.address = "a address"
-                this.phoneNumber = "010-1111-aaaa"
-            })
-
-            userList.add(UserRequest().apply {
-                this.name = "b"
-                this.age = 20
-                this.email = "b@gmail.com"
-                this.address = "b address"
-                this.phoneNumber = "010-1111-bbbb"
-            })
-
-            this.userRequest = userList
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+
+        return ResponseEntity.ok("")
+
+        //0. Response
+//        return UserResponse().apply {
+//            //1. request
+//            this.result = Result().apply{
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//        }.apply {
+//            this.description = "~~~~~~~~~~"
+//
+//        }.apply {
+//            val userList = mutableListOf<UserRequest>()
+//            userList.add(userRequest)
+//            userList.add(UserRequest().apply {
+//                this.name = "a"
+//                this.age = 10
+//                this.email = "a@gmail.com"
+//                this.address = "a address"
+//                this.phoneNumber = "010-1111-aaaa"
+//            })
+//
+//            userList.add(UserRequest().apply {
+//                this.name = "b"
+//                this.age = 20
+//                this.email = "b@gmail.com"
+//                this.address = "b address"
+//                this.phoneNumber = "010-1111-bbbb"
+//            })
+//
+//            this.userRequest = userList
+//        }
     }
 }
